c095b9d625e5082561ae539ba340f6baef00c6d6
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-10 17:28:29 +0200

    Add AuthModule to app.module.ts and create AuthController, AuthService, and their respective test files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database/database.service';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), TodosModule],
+  imports: [ConfigModule.forRoot({}), TodosModule, AuthModule],
   controllers: [],
   providers: [DatabaseService],
 })
