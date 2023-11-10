4da3623972728c3c178099b4e0f251a57fc76ce6
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-10 17:37:11 +0200

    Add UsersModule and UsersService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from './database/database.service';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({}), TodosModule, AuthModule],
+  imports: [ConfigModule.forRoot({}), TodosModule, AuthModule, UsersModule],
   controllers: [],
   providers: [DatabaseService],
 })
