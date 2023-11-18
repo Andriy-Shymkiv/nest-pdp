e5cb548e41afb9f271d8546a5fbd157de1128010
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-18 18:56:31 +0200

    Add JWT authentication to AuthModule

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserEntityService } from 'src/users/user-entity.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userEntityService: UserEntityService) {}
+  constructor(
+    private readonly userEntityService: UserEntityService,
+    private readonly jwtService: JwtService,
+  ) {}
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.userEntityService.findOne(username);
+    // @todo: add bcrypt
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload = { sub: user.userId, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
