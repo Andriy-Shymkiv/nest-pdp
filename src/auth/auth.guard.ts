6af0b30063c26b6c9765422db9c6bfe731a6f4b1
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-19 20:17:15 +0200

    Refactor authentication and role authorization logic

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,12 +4,9 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { ROLES_KEY } from 'src/common/roles.decorator';
 import { env } from 'src/config/env.validation';
-import { UserRole } from 'src/users/dto/user.dto';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -17,7 +14,6 @@ export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private userService: UsersService,
-    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -35,18 +31,10 @@ export class AuthGuard implements CanActivate {
       // 💡 We're assigning the user to the request object here
       // so that we can access it in our route handlers
       request['user'] = user;
-
-      // @todo: move to RolesGuard ???
-      const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
-        ROLES_KEY,
-        [context.getHandler(), context.getClass()],
-      );
-      return requiredRoles
-        ? requiredRoles.some((role) => user?.role.includes(role))
-        : true;
     } catch {
       throw new UnauthorizedException();
     }
+    return true;
   }
 
   private extractTokenFromHeader(request: Request): string | undefined {
