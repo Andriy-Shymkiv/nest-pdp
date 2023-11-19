b7827ee2cd4ab5d4c4b150b150214944f43cb7a7
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-19 20:22:29 +0200

    Add authentication guards to profile endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -29,6 +29,8 @@ export class AuthController {
     return this.authService.login(body);
   }
 
+  // will it be enough to do `@UseGuards(AuthGuard, RolesGuard)`?
+  // or need to register both of them at the module/controller level?
   @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(
