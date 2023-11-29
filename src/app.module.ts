582d65e4f1275fd3328502b35c1c11be8db771e0
Andriy-Shymkiv <winamondezik@gmail.com>
2023-11-29 22:39:00 +0200

    Add basic throttle module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Logger, Module } from '@nestjs/common';
 import ConfigModule from './config/config.module';
+import ThrottlerModule, {
+  throttlerProvider,
+} from './throttler/throttler.module';
 import { DatabaseService } from './database/database.service';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule, TodosModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule,
+    ThrottlerModule,
+    TodosModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
-  providers: [DatabaseService, Logger],
+  providers: [DatabaseService, Logger, throttlerProvider],
 })
 export class AppModule {}
