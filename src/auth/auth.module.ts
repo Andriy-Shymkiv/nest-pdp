a9d954ba311551e439bef18310e43536e7d84a01
Andriy-Shymkiv <winamondezik@gmail.com>
2023-12-03 18:12:01 +0200

    Update import statements in auth.guard.ts and auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,15 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { env } from 'src/config/env.validation';
 import { ACCESS_TOKEN_LIFETIME } from './common/constants';
+import { ENV } from 'src/config/config.module';
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
       global: true,
-      secret: env().JWT_SECRET,
+      secret: ENV.JWT_SECRET,
       signOptions: { expiresIn: ACCESS_TOKEN_LIFETIME },
     }),
   ],
