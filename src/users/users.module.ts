fca7a976aa479f041ea2b405db6ebc3a463e88ab
Andriy-Shymkiv <winamondezik@gmail.com>
2023-12-09 18:19:45 +0200

    Remove DatabaseService import from todos and users modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserEntityService } from './user-entity.service';
-import { DatabaseService } from 'src/database/database.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UserEntityService, DatabaseService],
+  providers: [UsersService, UserEntityService],
   exports: [UsersService, UserEntityService],
 })
 export class UsersModule {}
